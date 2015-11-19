97f88745de7f08de3e44edcacc7c6df607918da7
JudeMartin <judemartin.mj@gmail.com>
2015-11-18 19:21:52 -0800

    Added the room model

---
+++ src/main/java/sjsu/edu/cmpe275/service/RoomService.java
@@ -0,0 +1,12 @@
+package sjsu.edu.cmpe275.service;
+
+public class RoomService {
+	/*Room provisioning
+	Add a room. Must specify all the properties mentioned above.
+	Delete a room. A room cannot be removed from the system if it is in use or has a reservation.
+	For ease of testing, the system starts with 10 rooms with king beds (1 of them permits smoking),
+	 and 10 rooms with two queen beds (1 of them permits smoking).
+	 * 
+	 * */
+	
+}
