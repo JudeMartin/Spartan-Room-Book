cbed46bb422366b226e251dddd9c563f5b195141
waykar-prashant <waykar.prashant@gmail.com>
2015-11-21 15:37:06 -0800

    Added StringConstants Util class

---
+++ src/main/java/sjsu/edu/cmpe275/util/StringConstants.java
@@ -0,0 +1,14 @@
+package sjsu.edu.cmpe275.util;
+
+public class StringConstants {
+
+	//other type 1. smoking, 2. non-smoking
+	public static String OTHER_TYPE_SMOKING = "SMOKING";
+	public static String OTHER_TYPE_NON_SMOKING = "NON-SMOKING";
+	
+	//ROOM STATUS 0 - AVAILABLE, 1 - RESERVED, 2 - IN-USE
+	public static String STATUS_AVAILABLE = "Available";
+	public static String STATUS_RESERVED = "Reserved";
+	public static String STATUS_IN_USE = "In Use";
+	
+}
