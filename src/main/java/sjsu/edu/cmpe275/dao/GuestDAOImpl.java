633a3ff67a778774e5f6b43617cd038d7a51ef1c
JudeMartin <judemartin.mj@gmail.com>
2015-11-23 23:06:37 -0800

    code clean up before merge to master

--- src/main/java/sjsu/edu/cmpe275/dao/GuestDAOImpl.java
+++ src/main/java/sjsu/edu/cmpe275/dao/GuestDAOImpl.java
@@ -1,6 +1,5 @@
 package sjsu.edu.cmpe275.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -38,24 +37,11 @@ public class GuestDAOImpl implements GuestDAO {
 		// TODO Auto-generated method stub
 		System.out.println("In the ListGuest - DAO");
 		Session session = sessionFactory.openSession();
-
-		/*Query query = session.createQuery(" from guest");
-		List<Guest> listCategories1 = query.list();
-		 for (Guest guest : listCategories1) {
-			System.out.println(guest);
-		}
-		return listCategories1;
-		*/
-		
-		
 		try {
-			System.out.println("Try Block => In the ListGuest - DAO");		
 			List<Guest> res = (List<Guest>) session.createQuery("from Guest").list();
-			System.out.println("Try Block => after hibernate Sessoion In the ListGuest - DAO is list empty?"+res.isEmpty());	
 			return res;
 		} catch (HibernateException e) {
 			// TODO Auto-generated catch block
-			System.out.println("Caught the error in the ListGuests:");
 			e.printStackTrace();
 		}finally {
 			session.close();
