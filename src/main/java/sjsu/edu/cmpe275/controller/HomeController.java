35ec9569403a95d8af1ee564b6f1e037bd9cf08c
waykar-prashant <waykar.prashant@gmail.com>
2015-11-29 19:57:54 -0800

    updated home controller

--- src/main/java/sjsu/edu/cmpe275/controller/HomeController.java
+++ src/main/java/sjsu/edu/cmpe275/controller/HomeController.java
@@ -14,9 +14,38 @@ import sjsu.edu.cmpe275.model.RoomStatus;
 public class HomeController {
 
 	@RequestMapping(value="/")
-	public ModelAndView test(HttpServletResponse response) throws IOException{
+	public ModelAndView loadMain(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
 	}
 	
+	@RequestMapping(value="/home")
+	public ModelAndView loadHome(HttpServletResponse response) throws IOException{
+		return new ModelAndView("home");
+	}
 	
-}
+	@RequestMapping(value="/rooms")
+	public ModelAndView loadRooms(HttpServletResponse response) throws IOException{
+		return new ModelAndView("rooms");
+	}
+	
+	@RequestMapping(value="/facilities")
+	public ModelAndView loadFacilities(HttpServletResponse response) throws IOException{
+		return new ModelAndView("facilities");
+	}
+	
+	@RequestMapping(value="/gallery")
+	public ModelAndView loadGallery(HttpServletResponse response) throws IOException{
+		return new ModelAndView("gallery");
+	
+	}
+	
+	@RequestMapping(value="/map")
+	public ModelAndView loadMap(HttpServletResponse response) throws IOException{
+		return new ModelAndView("map");
+	}	
+	
+	@RequestMapping(value="/book-start")
+	public ModelAndView loadBookStart(HttpServletResponse response) throws IOException{
+		return new ModelAndView("book-start");
+	}
+}	
